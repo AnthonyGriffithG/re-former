afd2fcd1d2581095a6fe77d0fe2d0bec813d5471
Anthony Griffith <a.griffith.gonzalez@gmail.com>
2024-06-04 09:17:47 -0600

    add user form without rails helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def new
+    end
+
+    def create
+        new_user = User.new(user_params)
+        if (new_user.save)
+            redirect_to rails_health_check_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
